fa6de941215da3b7e0546504ff8d03bacdb6f7ad
ROHANROOSWELT <prohanrooswelt@gmail.com>
2026-07-25 00:19:18 +0530

    fix(casper): update event stream URL to HTTPS testnet endpoint

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ const sharedEnv = {
   ODRA_CASPER_LIVENET_NODE_ADDRESS: 'https://node.testnet.casper.network/rpc',
   ODRA_CASPER_LIVENET_CHAIN_NAME: 'casper-test',
   ODRA_CASPER_LIVENET_SECRET_KEY_PATH: '/home/azureuser/Desktop/keys/secret_key.pem',
-  ODRA_CASPER_LIVENET_EVENTS_URL: 'http://node.testnet.casper.network:9999/events/main',
+  ODRA_CASPER_LIVENET_EVENTS_URL: 'https://node.testnet.casper.network/events/main',
 };
 
 module.exports = {
